11ca9f3aa31fc7f93e411438b21ce43a62969d8e
Manuel Forneri <manuelforneri@hotmail.com>
2025-07-17 12:23:09 -0300

    Ejercicio 2

--- demo/src/main/java/manuel/forneri/Ejercicio2/Main.java
+++ demo/src/main/java/manuel/forneri/Ejercicio2/Main.java
@@ -1,5 +1,28 @@
 package manuel.forneri.Ejercicio2;
 
 public class Main {
+    public static void main(String[] args) {
+        MascotaVirtual mascota1 = new MascotaVirtual("Lola");
+        MascotaVirtual mascota2 = new MascotaVirtual("Mora");
 
+        System.out.println("---BIENVENIDO AL SIMULADOR DE MASCOTAS VIRTUALES---");
+
+        System.out.println("Estado inicial de las mascotas:");
+        mascota1.mostrarEstado();
+        System.out.println("");
+        mascota2.mostrarEstado();
+        System.out.println("");
+
+        System.out.println("Interactuando con " + mascota1.getNombre());
+        mascota1.alimentar();
+        mascota1.jugar();
+        mascota1.mostrarEstado();
+        System.out.println("");
+
+        System.out.println("Interactuando con " + mascota2.getNombre());
+        mascota2.jugar();
+        mascota2.alimentar();
+        mascota2.mostrarEstado();
+        System.out.println("");
+    }
 }
--- demo/src/main/java/manuel/forneri/Ejercicio2/MascotaVirtual.java
+++ demo/src/main/java/manuel/forneri/Ejercicio2/MascotaVirtual.java
@@ -1,5 +1,45 @@
 package manuel.forneri.Ejercicio2;
 
 public class MascotaVirtual {
+    private String nombre;
+    private int hambre;
+    private int felicidad;
 
+    // Constructor
+    public MascotaVirtual(String nombre) {
+        this.nombre = nombre;
+        this.hambre = 60;
+        this.felicidad = 60;
+    }
+
+    public void alimentar() {
+        this.hambre = Math.max(0, this.hambre - 20);
+        this.felicidad = Math.min(100, this.felicidad + 10);
+        System.out.println(nombre + " se ha alimentado.");
+    }
+
+    public void jugar() {
+        this.hambre = Math.min(100, this.hambre + 15);
+        this.felicidad = Math.min(100, this.felicidad + 20);
+        System.out.println(nombre + " ha jugado contigo y ahora es mas feliz.");
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Estado de " + nombre);
+        System.out.println("Hambre: " + this.hambre + "/100");
+        System.out.println("Felicidad: " + this.felicidad + "/100");
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHambre() {
+        return hambre;
+    }
+
+    public int getFelicidad() {
+        return felicidad;
+    }
 }
